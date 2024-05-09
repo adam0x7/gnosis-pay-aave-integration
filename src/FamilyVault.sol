d91f3d369519549d3bd9a36e42439a137223bba4
Adam Jackson <18746720+atj3097@users.noreply.github.com>
2024-05-09 11:59:05 -0500

    Update FamilyVault.sol

--- src/FamilyVault.sol
+++ src/FamilyVault.sol
@@ -1,19 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import {ERC4626} from "solmate/tokens/ERC4626.sol";
-import {ERC20} from "solmate/tokens/ERC20.sol";
+import {ERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
 
 contract FamilyVault is ERC4626 {
 
-    address wstETH = 0x6C76971f98945AE98dD7d4DFcA8711ebea946eA6;
+    IERC20 wstETH = IERC20(0x6C76971f98945AE98dD7d4DFcA8711ebea946eA6);
     address owner; //safe address
 
 
-    constructor(address _owner) ERC4626(IERC20(wstETH),
-                                "wstETH Family Shares",
-                                "wFS") {
+    constructor(address _owner) ERC4626(wstETH) ERC20("wstETH Family Shares", "wFS"){
         owner = _owner;
     }
 
