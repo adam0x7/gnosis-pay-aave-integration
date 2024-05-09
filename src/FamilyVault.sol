91aef637899852f1b604229292a78c393d5064f6
Adam Jackson <18746720+atj3097@users.noreply.github.com>
2024-05-09 14:31:21 -0500

    Update FamilyVault.sol

--- src/FamilyVault.sol
+++ src/FamilyVault.sol
@@ -5,6 +5,7 @@ import {ERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.so
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
 import {IPool} from "aave-v3-core/contracts/interfaces/IPool.sol";
+import {IPoolAddressesProvider} from "aave-v3-core/contracts/interfaces/IPoolAddressesProvider.sol";
 
 contract FamilyVault is ERC4626 {
 
@@ -16,7 +17,7 @@ contract FamilyVault is ERC4626 {
 
 
     uint256 public constant TWO_WEEKS = 1209600;
-    address owners; //safe address
+    address owner; //safe address
     uint256 riskTolerance;
 
     address[] public gpAccounts; //family accounts
@@ -24,16 +25,16 @@ contract FamilyVault is ERC4626 {
 
 
     constructor(address _owner,
-                address[] _gpAccounts,
+                address[] memory _gpAccounts,
                 uint256 _riskTolerance,
                 uint256 _aaveMarketId,
-                address owner) ERC4626(wstETH) ERC20("wstETH Family Shares", "wFS"){
+                address _aaveOwner) ERC4626(wstETH) ERC20("wstETH Family Shares", "wFS"){
         owner = _owner;
-        for(i = 0; i < gpAccounts.length; i++) {
+        for(uint256 i = 0; i < gpAccounts.length; i++) {
             gpAccounts.push(_gpAccounts[i]);
         }
         riskTolerance = _riskTolerance;
-        poolProvider = IPoolAddressesProvider(_aaveMarketId, owner);
+        poolProvider = IPoolAddressesProvider(_aaveMarketId, _aaveOwner);
         aavePool = IPool(poolProvider);
     }
 
@@ -42,7 +43,7 @@ contract FamilyVault is ERC4626 {
         _;
     }
 
-    function setAllowance(address calldata account, uint256 allowance) public {
+    function setAllowance(address account, uint256 allowance) public {
         require(allowance > 0);
         accountsToAllowances[account] = allowance;
     }
@@ -53,12 +54,12 @@ contract FamilyVault is ERC4626 {
     }
 
     function isTwoWeeksPassed(uint256 lastTimestamp) public view returns (bool) {
-        return (block.timestamp >= pastTimestamp + TWO_WEEKS);
+        return (block.timestamp >= lastTimestamp + TWO_WEEKS);
     }
 
     function getLoan() internal onlyOwner returns(bool) {
         currPayPeriod = block.timestamp;
-        aavePool.supply(address(wstEth), wstETH.balanceOf(address(this)) - 1, address(this), 0);
+        aavePool.supply(address(wstETH), wstETH.balanceOf(address(this)) - 1, address(this), 0);
         //how to calculate how much to borrow based off of weth??
         uint256 balanceBefore = eure.balanceOf(address(this));
         aavePool.borrow(address(eure), 10, 1, 0, address(this));
@@ -66,13 +67,13 @@ contract FamilyVault is ERC4626 {
         return eure.balanceOf(address(this)) > balanceBefore;
     }
 
-    //yield functions
-    function swapEUReForDAI() {
-        //happens with curve
-    }
-
-    function addLiquidityToBalancer() {
-        //add liquidity position to balancer, and remove liquidity
-    }
+//    //yield functions
+//    function swapEUReForDAI() {
+//        //happens with curve
+//    }
+//
+//    function addLiquidityToBalancer() {
+//        //add liquidity position to balancer, and remove liquidity
+//    }
 
 }
