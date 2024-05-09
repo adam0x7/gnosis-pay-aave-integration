a4991e749b0e872aeeac8caaa395c3e01cd2fd99
Adam Jackson <18746720+atj3097@users.noreply.github.com>
2024-05-09 12:30:48 -0500

    Update FamilyVault.sol

--- src/FamilyVault.sol
+++ src/FamilyVault.sol
@@ -9,17 +9,20 @@ contract FamilyVault is ERC4626 {
 
     IERC20 wstETH = IERC20(0x6C76971f98945AE98dD7d4DFcA8711ebea946eA6);
     address owners; //safe address
+    uint256 riskTolerance;
 
     address[] public gpAccounts; //family accounts
     mapping(address => uint256) public accountsToAllowances;
 
 
     constructor(address _owner,
-                address[] _gpAccounts) ERC4626(wstETH) ERC20("wstETH Family Shares", "wFS"){
+                address[] _gpAccounts,
+                uint256 _riskTolerance) ERC4626(wstETH) ERC20("wstETH Family Shares", "wFS"){
         owner = _owner;
         for(i = 0; i < gpAccounts.length; i++) {
             gpAccounts.push(_gpAccounts[i]);
         }
+        riskTolerance = _riskTolerance;
     }
 
     modifier onlyOwner() {
@@ -38,4 +41,15 @@ contract FamilyVault is ERC4626 {
     function getLoan() public {
 
     }
+
+    //yield functions
+
+    function swapEUReForDAI() {
+        //happens with curve
+    }
+
+    function addLiquidityToBalancer() {
+        //add liquidity position to balancer, and remove liquidity
+    }
+
 }
